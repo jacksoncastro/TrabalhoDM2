449f72194d4439562fb93b26ca231617281bcc9a
Francisco Jackson da Silva Castro <jack.vasc@yahoo.com.br>
2017-06-06 22:04:31 -0300

    Add question 2 and changes in question 3

---
+++ TrabalhoDM2/Question2/Window2.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrabalhoDM2.Question2
+{
+    /// <summary>
+    /// Lógica interna para Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private int count = 0;
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Counter.Content = String.Format("Estamos em {0}!", ++count);
+        }
+    }
+}
